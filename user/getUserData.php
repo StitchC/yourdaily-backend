e567e8f40dd4f4e5981b19ca8e8a37834261a3ba
stitchC <979987824@qq.com>
2017-09-09 23:11:13 +0800

    09-09后台对用户的数据输出完成

--- user/getUserData.php
+++ user/getUserData.php
@@ -6,7 +6,10 @@
 
 	$connect = $_GET[connectId];
 
-	$resultArr = array();
+	$resultArr = array(); // 保存最终数据
+	$daily = array(); // 保存对应日记内容数据
+	$info = array();  // 保存用户信息数据
+
 
 	$total_words = 0; // 保存所有日记的总字数
 
@@ -25,12 +28,28 @@
 
 	// 获取用户信息
 	$get_user_info = "select avatar, motto, username from user where id = '$id'";
+	
 
 	$daily_result = mysql_query($research_daily_sql);
 	$daily_count = mysql_query($daily_count_sql);
 	$user_info = mysql_query($get_user_info);
 
 	while ($rows = mysql_fetch_array($daily_result)) {
-		
+		$temp = array('id' => $rows[id], 'userId' => $rows[userId], 'title' => $rows[title], 'content' => $rows[content], 'mood' => $rows[mood], 'weather' => $rows[weather], 'publicTime' => $rows[publicTime]);
+
+		$total_words += mb_strlen($rows[content], "utf-8");
+
+		$daily[] = $temp;
 	}
+
+	while($infoItem = mysql_fetch_array($user_info)) {
+		$info = array('username' => $infoItem[username], 'avatar' => $infoItem[avatar], 'sex' => $infoItem[sex], 'motto' => $infoItem[motto], 'connect' => $infoItem[connect], 'totalWords' => $total_words);
+	}
+
+	$resultArr = array('daily' => $daily, 'userInfo' => $info);
+
+	echo json_encode($resultArr);
+	
+
+	
 ?>
