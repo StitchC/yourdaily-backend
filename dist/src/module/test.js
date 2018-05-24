4a263d5494803fda8f4a3e37dd66e76e2c971ade
stitch <979987824@qq.com>
2018-05-24 16:01:23 +0800

    新建分支

--- dist/src/module/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class Test {
-    constructor(thisname, thisage) {
-        this.name = thisname;
-        this.age = thisage;
-    }
-}
-//# sourceMappingURL=test.js.map
--- dist/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-router.get('/', function (req, res, next) {
-    res.send('hello express');
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
