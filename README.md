a655b381d5ec3b66a873f8765156cd1b0436a950
stitch <979987824@qq.com>
2018-05-27 10:14:57 +0800

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# yourdaily-backend
-backend support the yourdaily-webApp repository https://github.com/StitchC/yourDaily-webApp
--- src/module/test.ts
+++
@@ -1,9 +0,0 @@
-class Test {
-    public name: string
-    public age: number
-
-    constructor(thisname: string, thisage: number) {
-        this.name = thisname;
-        this.age = thisage;
-    }
-}
